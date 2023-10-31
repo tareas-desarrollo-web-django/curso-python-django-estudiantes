1cdbd17ef85f52862e5bfb35aaf789f3e0e53c7b
Marcos C. Vargas M <esocram@gmail.com>
2023-10-30 20:44:56 -0600

    Renombrar aplicación actividades -> practicas

--- Modulo 06/practicas/manage.py
+++ Modulo 06/practicas/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'actividades.settings.debug')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practicas.settings.debug')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- Modulo 06/practicas/practicas/wsgi.py
+++ Modulo 06/practicas/practicas/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for actividades project.
+WSGI config for practicas project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'actividades.settings.debug')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practicas.settings.debug')
 
 application = get_wsgi_application()
